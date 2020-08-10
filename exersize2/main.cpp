604fa06ad08b9d1b3ef0e923ff403b8085b84f46
haoxiaodian <haoxiaodian9527@163.com>
2020-08-10 21:33:37 +0800

    完善两个VAO和VBO在同一程序中绘制两个三角形

--- exersize2/main.cpp
+++ exersize2/main.cpp
@@ -12,6 +12,13 @@ const char * pVertexShader = "#version 330 core\n"
 "	gl_Position = vec4(apos.x,apos.y,apos.z,1.0f);\n"
 "}\n";
 
+const char * pFragmentShader = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 1.0f, 0.0f,1.0f);\n"
+"}\n";
+
 int main()
 {
 	//glfw初始化
@@ -53,7 +60,70 @@ int main()
 		std::cout << "ERROR::SHADER::VERTEX::COMPILE_FAILE\n" << cInfoLog << std::endl;
 	}
 
+	//创建并编译片段着色器
+	unsigned int unFragementShader = 0;
+	unFragementShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(unFragementShader,1,&pFragmentShader,NULL);
+	glCompileShader(unFragementShader);
+
+	int nSucess1 = 0;
+	char cInfoLog1[512] = { 0 };
+	glGetShaderiv(unFragementShader,GL_COMPILE_STATUS,&nSucess1);
+	if (!nSucess1)
+	{
+		glGetShaderInfoLog(unFragementShader, 512, NULL, cInfoLog1);
+		std::cout << "ERROR::SHADER::FRAGMENT_COMPILE_FAILE\n" << cInfoLog1<< std::endl;
+	}
+
+	//创建并链接着色程序
+	unsigned int unShaderProgram = 0;
+	unShaderProgram = glCreateProgram();
+	glAttachShader(unShaderProgram,unVertexShader);
+	glAttachShader(unShaderProgram, unFragementShader);
+	glLinkProgram(unShaderProgram);
+
+	int nSuccess2 = 0;
+	char cInfoLog2[512] = { 0 };
+	glGetProgramiv(unShaderProgram, GL_LINK_STATUS, &nSuccess2);
+	if (!nSuccess2)
+	{
+		glGetProgramInfoLog(unShaderProgram, 512, NULL, cInfoLog2);
+		std::cout << "ERROR::PROGRAM_LINK_FAILE\n" << cInfoLog2 << std::endl;
+	}
+
 	glDeleteShader(unVertexShader);
+	glDeleteShader(unFragementShader);
+
+	float vertices1[] = {
+		-0.5f,0.5f,0.0f,
+		0.0f,0.5f,0.0f,
+		-0.5f,0.0f,0.0f
+	};
+
+	float vertices2[] = {
+		0.0f,0.0f,0.0f,
+		0.5f,0.0f,0.0f,
+		0.0f,-0.5f,0.0f
+	};
+
+	unsigned int VAO[2], VBO[2];
+	glGenVertexArrays(2, VAO);
+	glGenBuffers(2, VBO);
+
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 0,(void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(pWindow))
 	{
@@ -61,10 +131,19 @@ int main()
 		glClearColor(0.2f,0.3f,0.3f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(unShaderProgram);
+		glBindVertexArray(VAO[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(pWindow);
 		glfwPollEvents();
 	}
 	
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
+	glDeleteProgram(unShaderProgram);
 	glfwTerminate();
 	return 0;
 }
