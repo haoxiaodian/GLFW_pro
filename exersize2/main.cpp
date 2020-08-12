5ca6d889991bace75082d17cd7e4edce5e3acd43
haoxiaodian <haoxiaodian9527@163.com>
2020-08-12 09:16:28 +0800

    多属性的使用

--- exersize2/main.cpp
+++ exersize2/main.cpp
@@ -7,16 +7,20 @@ void processInput(GLFWwindow*pWindow);
 
 const char * pVertexShader = "#version 330 core\n"
 "layout(location = 0) in vec3 apos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"out vec3 newColor;\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(apos.x,apos.y,apos.z,1.0f);\n"
+"	newColor = aColor;\n"
 "}\n";
 
 const char * pFragmentShader1 = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 newColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 1.0f, 0.0f,1.0f);\n"
+"	FragColor = vec4(newColor,1.0f);\n"
 "}\n";
 
 const char * pFragmentShader2 = "#version 330 core\n"
@@ -134,9 +138,9 @@ int main()
 	glDeleteShader(unFragmentShader2);
 
 	float vertices1[] = {
-		-0.5f,0.5f,0.0f,
-		0.0f,0.5f,0.0f,
-		-0.5f,0.0f,0.0f
+		-0.5f,0.5f,0.0f,	1.0f,0.0f,0.0f,
+		0.0f,0.5f,0.0f,		0.0f,1.0f,0.0f,
+		-0.5f,0.0f,0.0f,	0.0f,0.0f,1.0f
 	};
 
 	float vertices2[] = {
@@ -152,8 +156,10 @@ int main()
 	glBindVertexArray(VAO[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)0);//设置位置属性
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)(3*sizeof(float)));//设置颜色属性
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
